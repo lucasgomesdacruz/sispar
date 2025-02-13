fe5f264588ecc46c0d7e706d3fd84d302c0caf02
Lucas Gomes <lucaass1997rp@gmail.com>
2025-02-13 18:00:55 -0300

    :broom: cleanup: retirar header de dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,5 @@
 import styles from "./Dashboard.module.scss"
-import { AiOutlineHome } from "react-icons/ai";
-// import { FaListUl } from "react-icons/fa6";
+
 import { IoIosList } from "react-icons/io";
 import { VscHistory } from "react-icons/vsc";
 import { BsClipboardData } from "react-icons/bs";
@@ -16,27 +15,16 @@ import { TbArrowBackUp } from "react-icons/tb";
 // import { FiCheck } from "react-icons/fi";
 import { MdOutlineFileDownloadDone } from "react-icons/md";
 import { BsX } from "react-icons/bs";
-
-
-
-
+import { VscCloudUpload } from "react-icons/vsc";
 
 function Dashboard() {
     return (
         <>
         <div className={styles.dashboard}>
-            <header className={styles.header}>    
-                <AiOutlineHome />
 
-                <nav>
-                    <ul>
-                        <li>Reembolsos</li>
-                    </ul>
-                </nav>
-            </header>
             <main className={styles.mainContainer}>
                 <div className={styles.mainHeader}>
-                    <h1>Sistema de reembolsos</h1>
+                    <h1>Sistema de Reembolsos</h1>
                     <p>Solicite novos pedidos de reembolso, visualize solicitações em análise e todo o histórico.</p>
                 </div>
                 
@@ -69,7 +57,7 @@ function Dashboard() {
                         </li>
                     </ul>
 
-                    <p className={styles.sistem}>Sitema atualizado</p>
+                    <p className={styles.sistem}><VscCloudUpload /> Sistema atualizado</p>
                 </section>
             </main>
         </div>
