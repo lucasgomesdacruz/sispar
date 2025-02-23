7c5c0261c1baeb472c87c78d7be0788c72d343d4
Lucas Gomes <lucaass1997rp@gmail.com>
2025-02-23 13:59:19 -0300

    :sparkles: feat: criar seção de ainda em desenvolvimento

--- src/pages/analises/Analises.jsx
+++ src/pages/analises/Analises.jsx
@@ -8,9 +8,20 @@ function Analises() {
         <main className={styles.analises}>
             <Header  icon={<MdOutlineNavigateNext />} text="Análises"/>
 
-            <div>
-                <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People%20with%20professions/Construction%20Worker%20Light%20Skin%20Tone.png" alt="Construction Worker Light Skin Tone" width="25" height="25" />
-                <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Construction.png" alt="Construction" width="25" height="25" />
+            <div className={styles.constructionContainer}>
+                <img 
+                    src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People%20with%20professions/Construction%20Worker%20Light%20Skin%20Tone.png" 
+                    alt="Construction Worker" 
+                    width="50" 
+                    height="50" 
+                />
+                <img 
+                    src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Construction.png" 
+                    alt="Construction Sign" 
+                    width="50" 
+                    height="50" 
+                />
+                <p className={styles.constructionText}>Esta seção ainda está em construção. Em breve teremos novidades!</p>
             </div>
             
         </main>
--- src/pages/historico/Historico.jsx
+++ src/pages/historico/Historico.jsx
@@ -1,20 +1,29 @@
-import styles from "./Historico.module.scss"
-import Header from "../../components/header/Header.jsx"
-import { MdOutlineNavigateNext } from "react-icons/md"
-
+import styles from "./Historico.module.scss";
+import Header from "../../components/header/Header.jsx";
+import { MdOutlineNavigateNext } from "react-icons/md";
 
 function Historico() {
     return (
         <main className={styles.historico}>
-            <Header  icon={<MdOutlineNavigateNext />} text="Historico"/>
-
-            <div>
-                <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People%20with%20professions/Construction%20Worker%20Light%20Skin%20Tone.png" alt="Construction Worker Light Skin Tone" width="25" height="25" />
-                <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Construction.png" alt="Construction" width="25" height="25" />
-            </div>
+            <Header icon={<MdOutlineNavigateNext />} text="Histórico" />
             
+            <div className={styles.constructionContainer}>
+                <img 
+                    src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People%20with%20professions/Construction%20Worker%20Light%20Skin%20Tone.png" 
+                    alt="Construction Worker" 
+                    width="50" 
+                    height="50" 
+                />
+                <img 
+                    src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Construction.png" 
+                    alt="Construction Sign" 
+                    width="50" 
+                    height="50" 
+                />
+                <p className={styles.constructionText}>Esta seção ainda está em construção. Em breve teremos novidades!</p>
+            </div>
         </main>
-    )
+    );
 }
 
-export default Historico
+export default Historico;
