b37423a6821f2bfdf37c845386688aa2c8cc6074
Lucas Gomes <lucaass1997rp@gmail.com>
2025-02-25 11:58:50 -0300

    :sparkles: feat: adiciona conteudo em novas rotas

--- src/pages/criarConta/Criar.jsx
+++ src/pages/criarConta/Criar.jsx
@@ -1,7 +1,25 @@
-
+import styles from "./criar.module.scss"
 function Criar() {
     return (
-        <h1>criar conta</h1>
+        <main className={styles.create}>
+            
+            
+            <div className={styles.constructionContainer}>
+                <img 
+                    src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People%20with%20professions/Construction%20Worker%20Light%20Skin%20Tone.png" 
+                    alt="Construction Worker" 
+                    width="50" 
+                    height="50" 
+                />
+                <img 
+                    src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Construction.png" 
+                    alt="Construction Sign" 
+                    width="50" 
+                    height="50" 
+                />
+                <p className={styles.constructionText}>Esta seção ainda está em construção. Em breve teremos novidades!</p>
+            </div>
+        </main>
     )
 }
 
