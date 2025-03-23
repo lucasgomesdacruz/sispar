ccf645a2b8d36aceeb3ea4e5420f44a123cef8d5
Lucas Gomes <lucaass1997rp@gmail.com>
2025-03-23 17:05:45 -0300

    :lipstick: feat: adiciona formulário de criar conta

--- src/pages/criarConta/Criar.jsx
+++ src/pages/criarConta/Criar.jsx
@@ -1,7 +1,16 @@
 import { Helmet } from "react-helmet-async";
-import Construction from "../../components/construction/Construction.jsx";
 import styles from "./Criar.module.scss"
+import Input from "../../components/Input/Input.jsx";
+import logo from "../../assets/images/TelaLogin/logo.png"
+import Button from "../../components/button/Button.jsx";
+
 function Criar() {
+    
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        alert("Form enviado")
+    }
+
     return (
         <>
             <Helmet>
@@ -35,7 +44,28 @@ function Criar() {
                 <meta name="twitter:card" content="summary_large_image" />
             </Helmet>
             <main className={styles.create}>
-                <Construction />
+                <div>
+                    <img src={logo} alt="Lodo da wison sons" />
+                </div>
+                <form action="" onSubmit={handleSubmit}>
+                    <fieldset>
+                        <Input type="text" label="Nome completo"/>
+                        <div className={styles.container}>
+                            <Input type="text" label="CPF"/>
+                            <Input type="text" label="Matricula"/>
+                            <Input type="date" label="Data de nascimento" placeholder="(DD/MM/AAAA)"/>
+                        </div>
+                        
+                        <Input type="text" label="Telefone" placeholder="(00)0000000000"/>
+                        <Input type="text" label="Email"/>
+                        <Input type="text" label="Confirmar Email"/>
+                        <Input type="password" label="Senha"/>
+                        <Input type="password" label="Confirmar senha"/>
+                    </fieldset>
+
+                    <Button text="Entrar" type="submit" className={styles.btnDark} />
+                    <p className={styles.enter}>Ja possui uma conta? Entrar</p>
+                </form>
             </main>
         </>
         
