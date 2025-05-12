a741244726f389e61cfe1df6e5a7dfdafa651619
Lucas Gomes <lucaass1997rp@gmail.com>
2025-05-11 21:57:16 -0300

    feat: adiciona consumo de api em historico

--- src/pages/historico/Historico.jsx
+++ src/pages/historico/Historico.jsx
@@ -1,10 +1,35 @@
 import styles from "./Historico.module.scss";
+import logo from "../../assets/images/TelaLogin/logo.png"
+
 import Header from "../../components/header/Header.jsx";
 import { MdOutlineNavigateNext } from "react-icons/md";
-import Construction from "../../components/construction/Construction.jsx";
 import { Helmet } from "react-helmet-async";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+import Api from "../../Services/Api.jsx";
+import { useEffect, useState } from "react";
+import { IoDocumentTextSharp } from "react-icons/io5";
+
+
 
 function Historico() {
+    const [reembolsos, setReembolsos] = useState([]);
+   
+    const fetchReembolsos = async () => {
+        try {
+            const response = await Api.get('/colaborador/reembolsos');
+            console.log("Dados recebidos:", response.data);
+            setReembolsos(response.data);
+        } catch (err) {
+            console.error("Erro ao buscar reembolsos:", err);
+           
+        }
+    };
+
+    useEffect(() => {
+        fetchReembolsos();
+    }, []);
+
+
     return (
         <>
             <Helmet>
@@ -40,7 +65,62 @@ function Historico() {
             <main className={styles.historico}>
                 <Header icon={<MdOutlineNavigateNext />} text="Histórico" />
                 
-                <Construction />
+                <section className={styles.containerTitle}>
+                    <img src={logo} alt="Logo" className={styles.logo} />
+                    <h1 className={styles.title}>Consulte seu Histórico de reembolsos</h1>
+                    <p className={styles.subtitle}>Sistema de Emissão de Boletos e Parcelamento</p>
+                </section>
+                
+                <section className={styles.tableContainer}>
+                    <table className={styles.customTable}>
+                        <thead className={styles.containerThead}>
+                            <tr className={styles.containerTr}>
+                                <th><IoIosInformationCircleOutline /></th>
+                                <th>Colaborador(a)</th>
+                                <th>Empresa</th>
+                                <th>N° Prest.</th>
+                                <th>Data</th>
+                                <th>Motivo</th>
+                                <th>Tipo Reemb.</th>
+                                <th>Ctr. Custo</th>
+                                <th>Ord. Int.</th>
+                                <th>Div.</th>
+                                <th>PEP</th>
+                                <th>Moeda</th>
+                                <th>Dist. Km</th>
+                                <th>Val. Km</th>
+                                <th>Val. Faturado</th>
+                                <th>Despesa</th>
+                            </tr>
+                        </thead>
+                        <tbody className={styles.containerTbody}>
+                            {reembolsos.map((task, index) => (
+                                <tr key={index} className={styles.containerTr}>
+                                    <td className={styles.clickHover}>
+                                        <IoIosInformationCircleOutline />
+                                    </td>
+
+                                    <td>{task.colaborador}</td>
+                                    <td>{task.empresa}</td>
+                                    <td>{task.num_prestacao}</td>
+                                    <td>{task.data}</td>
+                                    <td className={styles.motiveHover}><IoDocumentTextSharp /></td>
+                                    <td>{task.tipo_reembolso}</td>
+                                    <td>{task.centro_custo}</td>
+                                    <td>{task.ordem_interna}</td>
+                                    <td>{task.divisao}</td>
+                                    <td>{task.pep}</td>
+                                    <td>{task.moeda}</td>
+                                    <td>{task.distancia_km}</td>
+                                    <td>{task.valor_km}</td>
+                                    <td>{task.valor_faturado}</td>
+                                    <td>{task.despesa}</td>
+                                </tr>
+                            ))}
+                            
+                        </tbody>
+                    </table>
+                </section>
             </main>
         </>
         
