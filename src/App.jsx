bf5870386cb105ae0f8a43ff70f2a47dab6a2591
Lucas Gomes <lucaass1997rp@gmail.com>
2025-05-26 16:03:13 -0300

    fix: erro no nome da pasta de dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ import Perfil from "./pages/perfil/Perfil.jsx";
 import Helpdesk from "./pages/helpDesk/Helpdesk.jsx";
 import LoginAdm from "./pages/LoginAdm/LoginAdm.jsx";
 import LayoutAdm from "./components/Layouts/LayoutAdm.jsx";
-import DashboardAdm from "./pages/dashboardAmd/dashboardAdm.jsx";
+import DashboardAdm from "./pages/dashboardAdm/dashboardAdm.jsx";
+
 
 
 const router = createBrowserRouter([
